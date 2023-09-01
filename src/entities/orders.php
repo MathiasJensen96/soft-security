1efa44daf5a75df69c4573f669b8162d1831908f
Mathias Jensen <mathias@Mathiass-MacBook-Pro.local>
2023-09-01 10:27:46 +0200

    entities

---
+++ src/entities/orders.php
@@ -0,0 +1,88 @@
+<?php
+    class orders {
+        public $id;
+        public $status;
+        public $date;
+        public $user_email;
+
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of status
+         */ 
+        public function getStatus()
+        {
+                return $this->status;
+        }
+
+        /**
+         * Set the value of status
+         *
+         * @return  self
+         */ 
+        public function setStatus($status)
+        {
+                $this->status = $status;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of date
+         */ 
+        public function getDate()
+        {
+                return $this->date;
+        }
+
+        /**
+         * Set the value of date
+         *
+         * @return  self
+         */ 
+        public function setDate($date)
+        {
+                $this->date = $date;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of user_email
+         */ 
+        public function getUser_email()
+        {
+                return $this->user_email;
+        }
+
+        /**
+         * Set the value of user_email
+         *
+         * @return  self
+         */ 
+        public function setUser_email($user_email)
+        {
+                $this->user_email = $user_email;
+
+                return $this;
+        }
+    }
+?>
---
+++ src/entities/users.php
@@ -0,0 +1,75 @@
+<?php
+    class users {
+        //Properties
+        public $email;
+        public $password;
+        public $role;
+
+        //Methods
+        public function __construct($email, $password, $role) {
+            $this->email = $email;
+            $this->password = $password;
+            $this->role = $role;
+        }
+
+        /**
+         * Get the value of email
+         */ 
+        public function getEmail()
+        {
+                return $this->email;
+        }
+
+        /**
+         * Set the value of email
+         *
+         * @return  self
+         */ 
+        public function setEmail($email)
+        {
+                $this->email = $email;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+                $this->password = $password;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of role
+         */ 
+        public function getRole()
+        {
+                return $this->role;
+        }
+
+        /**
+         * Set the value of role
+         *
+         * @return  self
+         */ 
+        public function setRole($role)
+        {
+                $this->role = $role;
+
+                return $this;
+        }
+    }
+?>
