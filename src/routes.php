4dc3eedc40b13e698c241f87012505dd8d18f649
Mads Højris Kristensen <cph-mk613@cphbusiness.dk>
2023-09-04 11:04:21 +0200

    added two routes login and register

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,6 @@
 
 require_once __DIR__ . '/router.php';
 
-
+post('/login', 'views/login.php'); //endpoint er /login og den tager dig til login.php siden
+post('/register', 'views/register.php');
 any('/404','views/404.php');
