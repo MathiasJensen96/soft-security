dadb465448668212ef8615bf3bb9554367933dad
Mathias Jensen <mathias@Mathiass-MacBook-Pro.local>
2023-09-05 08:57:11 +0200

    read from db

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/router.php';
 get('/', 'views/index.php');
 get('/login', 'views/login.php');
 get('/register', 'views/register.php');
+get('/products', 'views/test.php');
 post('/login', 'views/verify.php');
 post('/register', 'views/create_user.php');
 any('/404','views/404.php');
