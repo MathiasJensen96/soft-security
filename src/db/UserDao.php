15dd4e23775e7c5872ecba1c4e8edf7743ed7f80
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-05 13:27:55 +0200

    userdao refactor

--- src/db/UserDao.php
+++ src/db/UserDao.php
@@ -16,8 +16,7 @@ class UserDao
 
     function getUser(string $email)
     {
-        global $userconn;
-        $stm = $userconn->prepare("select * from user where email = ?");
+        $stm = $this->userconn->prepare("select * from user where email = ?");
         $stm->execute([$email]);
         $user = $stm->fetch(PDO::FETCH_ASSOC);
         if (!$user) {
@@ -26,9 +25,11 @@ class UserDao
         return new users($user['email'], $user['password'], $user['role']);
     }
     function createUser(users $user){
-        global $userconn;
-        $sqlcreate = $userconn->prepare("insert into user (`email`,`password`,`role`) values (?,?,?)");
-        return $sqlcreate->execute([$user->getEmail(),$user->getPassword(),$user->getRole()]);
-        
+        $sql = "insert into user (`email`,`password`,`role`) values (:email, :password, :role)";
+        $stmt = $this->userconn->prepare($sql);
+        $stmt->bindValue('email', $user->getEmail());
+        $stmt->bindValue('password', $user->getPassword());
+        $stmt->bindValue('role', $user->getRole());
+        return $stmt->execute();
     }
 }
