80e47859fc21fc944be5cfed4e69dd561370adfe
Mads Højris Kristensen <cph-mk613@cphbusiness.dk>
2023-09-05 13:19:43 +0200

    more refactor reformat

--- src/services/signup.serv.php
+++ src/services/signup.serv.php
@@ -5,18 +5,24 @@ $userDao = new UserDao();
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST["email"];
-    $pword = password_hash($_POST["password"], PASSWORD_DEFAULT);
-    $user = new users($email, $pword, "user");
-    //print_r($user); //debug line
-    if ($userDao->getUser($user->getEmail())) {
-        echo "A user with that email already exists";
+    $password = $_POST["password"];
+    
+    if (empty($email) || empty($password)) {
+        return http_response_code(400);
     } else {
-        if ($userDao->createUser($user)) {
-            return http_response_code(204);
+        $pword = password_hash($password, PASSWORD_DEFAULT);
+        $user = new users($email, $pword, "user");
+
+        if ($userDao->getUser($user->getEmail())) {
+            echo "A user with that email already exists";
         } else {
-            //TODO: ERROR HANDLING
-            echo "something went wrong";
-            return http_response_code(400);
+            if ($userDao->createUser($user)) {
+                return http_response_code(204);
+            } else {
+                //TODO: ERROR HANDLING
+                echo "something went wrong";
+                return http_response_code(400);
+            }
         }
     }
 }
