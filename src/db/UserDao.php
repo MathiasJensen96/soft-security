7ad79cac8daa245064c89622a367fa96a0a5c65a
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-05 10:50:40 +0200

    login with service and user dao

---
+++ src/db/UserDao.php
@@ -0,0 +1,28 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/entities/users.php';
+
+class UserDao
+{
+    private $userconn;
+    private $adminconn;
+
+    function __construct()
+    {
+        global $userconn, $adminconn;
+        include 'dbconn.php';
+        $this->userconn = $userconn;
+        $this->adminconn = $adminconn;
+    }
+
+    function getUser(string $email)
+    {
+        global $userconn;
+        $stm = $userconn->prepare("select * from user where email = ?");
+        $stm->execute([$email]);
+        $user = $stm->fetch(PDO::FETCH_ASSOC);
+        if (!$user) {
+            return null;
+        }
+        return new users($user['email'], $user['password'], $user['role']);
+    }
+}
--- src/services/login.php
+++ src/services/login.php
@@ -1,15 +1,16 @@
-<?php include __DIR__ . "/../db/dbconn.php"?>
-<?php
+<?php include __DIR__ . "/../db/UserDao.php";
+
 session_start();
-    $password = $_POST['password'];
-    $stm = $userconn->prepare("select * from user where email = ?");
-    $stm->execute([$_POST['email']]);
-    $user = $stm->fetch(PDO::FETCH_ASSOC);
-    //lav en user class til at være = $user
-    print_r($user); //debug
-    if ($user){
-      if (password_verify($password, $user['password'])){
-        echo "<h1>hurraaa</h1>";
-        // set session variables here i guess
-        }  
-    }
+
+$password = $_POST['password'];
+$userDao = new UserDao();
+$user = $userDao->getUser($_POST['email']);
+if ($user && password_verify($password, $user->password)) {
+    session_regenerate_id();
+    $_SESSION['email'] = $user->email;
+    $_SESSION['role'] = $user->role;
+
+    echo "<h1>Welcome</h1>";
+} else {
+    echo 'Incorrect username or password.';
+}
