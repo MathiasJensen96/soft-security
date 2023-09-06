768501f5dcc3b544416034a4eccf5f5e3417303f
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-07 00:24:49 +0200

    adds session validation to adminpage

--- src/controllers/adminPageController.php
+++ src/controllers/adminPageController.php
@@ -1,12 +1,16 @@
 <?php
+require_once __DIR__ . '/../security/AuthenticationManager.php';
 require_once __DIR__ . '/../security/AuthorizationManager.php';
 
+use security\AuthenticationManager;
 use security\AuthorizationManager;
 
 session_start();
 
+$authenticationManager = new AuthenticationManager();
 $authorizationManager = new AuthorizationManager();
-if (!$authorizationManager->requireRole('admin')) {
+
+if (!$authenticationManager->validateSession() || !$authorizationManager->requireRole('admin')) {
     header('Location: /login');
 } else {
     require_once __DIR__ . '/../views/adminpage.php';
