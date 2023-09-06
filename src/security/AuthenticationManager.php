171d221749dfd5b8b734c03ec2e476f926432d92
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-06 14:03:47 +0200

    session expiration

--- src/security/AuthenticationManager.php
+++ src/security/AuthenticationManager.php
@@ -4,6 +4,9 @@ namespace security;
 
 class AuthenticationManager
 {
+    const ABSOLUTE_EXPIRATION = 60 * 60 * 4; // 4 hours
+    const IDLE_EXPIRATION = 60 * 30; // 30 minutes
+
     function validateSession(): bool
     {
         return !$this->isExpired();
@@ -11,12 +14,20 @@ class AuthenticationManager
 
     function isExpired(): bool
     {
-        return isset($_SESSION['expiration']) && $_SESSION['expiration'] <= time();
+        return (isset($_SESSION['expiration']) && $_SESSION['expiration'] <= time())
+            || (isset($_SESSION['lastActive']) && $_SESSION['lastActive'] + self::IDLE_EXPIRATION <= time());
     }
 
     function invalidateSession(): void
     {
         $_SESSION['expiration'] = time() + 10;
         setcookie(session_name(), "");
+    }
+
+    function regenerateSession(): void
+    {
+        session_regenerate_id();
+        $_SESSION['expiration'] = time() + self::ABSOLUTE_EXPIRATION;
+        $_SESSION['lastActive'] = time();
     }
 }
--- src/services/adminResource.php
+++ src/services/adminResource.php
@@ -1,13 +1,21 @@
 <?php
 
+require_once __DIR__ . '/../security/AuthenticationManager.php';
 require_once __DIR__ . '/../security/AuthorizationManager.php';
-include '../db/UserDao.php';
+require_once __DIR__ . '/../db/UserDao.php';
 
+use security\AuthenticationManager;
 use security\AuthorizationManager;
 
 session_start();
 
+$authenticationManager = new AuthenticationManager();
 $authorizationManager = new AuthorizationManager();
+
+if ($authenticationManager->isExpired()) {
+    echo "Your session has expired.";
+    exit;
+}
 if (!$authorizationManager->requireRole('admin')) {
     echo "You are not authorised!";
     return http_response_code(403);
