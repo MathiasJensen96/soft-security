7d79583aee0f7f2e44a27aaf91c9701abbb1cc8e
Mads Højris Kristensen <cph-mk613@cphbusiness.dk>
2023-09-07 10:59:22 +0200

    comments and error logging when trying to login

--- src/services/signup.serv.php
+++ src/services/signup.serv.php
@@ -6,13 +6,16 @@ $userDao = new UserDao();
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
-    
+
+    // Checks if email or password is empty
     if (empty($email) || empty($password)) {
+        echo "Please fill out both fields";
         return http_response_code(400);
     } else {
         $pword = password_hash($password, PASSWORD_ARGON2ID);
         $user = new users($email, $pword, "user");
 
+        // Checks if the user already exists
         if ($userDao->getUser($user->getEmail())) {
             echo "Something went wrong";
             return http_response_code(409);
@@ -21,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 return http_response_code(204);
             } else {
                 //TODO: ERROR HANDLING
-                echo "something went wrong";
+                echo "Something went wrong on the server, try again later";
                 return http_response_code(400);
             }
         }
