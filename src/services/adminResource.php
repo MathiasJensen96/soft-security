c65deb7937f8abd95c28c6e4e6bbb630a942f240
Mads Højris Kristensen <cph-mk613@cphbusiness.dk>
2023-09-07 15:12:45 +0200

    moved logging file-name to env file need to rebuild docker for it to work

--- src/services/adminResource.php
+++ src/services/adminResource.php
@@ -34,7 +34,7 @@ if (!$authorizationManager->requireRole('admin')) {
             header("Content-Type: JSON");
             $user = $userDao->getUser($userEmail);
             echo json_encode($user, JSON_PRETTY_PRINT);
-            error_log(date('l jS \of F Y h:i:s A')." | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " succesfully used 'get user' endpoint and found: " . json_encode($user) . "\n", 3, "admin_endpoint.log");
+            error_log(date('l jS \of F Y h:i:s A')." | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " succesfully used 'get user' endpoint and found: " . json_encode($user) . "\n", 3, $_ENV['ADMIN_ENDPOINT_LOG']);
         } else {
             error_log("User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " tried to find a non-existent user: " . $userEmail);
             echo "User doesn't exist";
@@ -55,7 +55,7 @@ if (!$authorizationManager->requireRole('admin')) {
                     $userDao->updateUser($newEmail, $role, $email);
                     $updatedUser = $userDao->getUser($newEmail);
                     http_response_code(200);
-                    error_log(date('l jS \of F Y h:i:s A')." | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " succesfully used 'update user' endpoint, updated user : " . json_encode($updatedUser) . "\n", 3, "admin_endpoint.log");
+                    error_log(date('l jS \of F Y h:i:s A')." | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " succesfully used 'update user' endpoint, updated user : " . json_encode($updatedUser) . "\n", 3, $_ENV['ADMIN_ENDPOINT_LOG']);
                     return $updatedUser;
                 } else {
                     error_log("User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " tried to update information of non-existent user: " . $email);
@@ -74,7 +74,7 @@ if (!$authorizationManager->requireRole('admin')) {
                 if ($userDao->getUser($deleteUserEmail)) {
 
                     $userDao->deleteUser($deleteUserEmail);
-                    error_log(date('l jS \of F Y h:i:s A')." | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " succesfully used 'delete user' endpoint and deleted: " . $deleteUserEmail . "\n", 3, "admin_endpoint.log");
+                    error_log(date('l jS \of F Y h:i:s A')." | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " succesfully used 'delete user' endpoint and deleted: " . $deleteUserEmail . "\n", 3, $_ENV['ADMIN_ENDPOINT_LOG']);
                     return http_response_code(200);
                 } else {
                     error_log("User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " tried to update information of non-existent user: " . $deleteUserEmail);
