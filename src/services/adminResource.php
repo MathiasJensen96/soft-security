9fa6f04e1c9a1eaa632e03c80411af39f2977dd9
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-09 01:20:30 +0200

    user not found response

--- src/services/adminResource.php
+++ src/services/adminResource.php
@@ -2,10 +2,12 @@
 
 require_once __DIR__ . '/../controllers/AccessController.php';
 require_once __DIR__ . '/../db/UserDao.php';
+require_once __DIR__ . '/../error_handling/ErrorResponse.php';
 require_once __DIR__ . '/../security/InputValidator.php';
 
 
 use controllers\AccessController;
+use error_handling\ErrorResponse;
 use security\InputValidator;
 
 session_start();
@@ -21,14 +23,18 @@ if ($_SERVER['REQUEST_METHOD'] === "GET") {
 
     $userEmail = $_GET['userEmail'];
     if ($inputValidator->email($userEmail)) {
-
-        header("Content-Type: application/json");
         $user = $userDao->getUser($userEmail);
+        if (empty($user)) {
+            error_log("User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " tried to find non-existent user: " . $userEmail);
+            ErrorResponse::makeErrorResponse(404, "User not found with email: $userEmail");
+            exit;
+        }
+        header("Content-Type: application/json");
         echo json_encode($user, JSON_PRETTY_PRINT);
-        error_log(date('l jS \of F Y h:i:s A') . " | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " succesfully used 'get user' endpoint and found: " . json_encode($user) . "\n", 3, $_ENV['ADMIN_ENDPOINT_LOG']);
+        error_log(date('l jS \of F Y h:i:s A') . " | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " successfully used 'get user' endpoint and found: " . json_encode($user) . "\n", 3, $_ENV['ADMIN_ENDPOINT_LOG']);
     } else {
-        error_log("User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " tried to find a non-existent user: " . $userEmail);
-        echo "User doesn't exist";
+        error_log("User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " tried to find user with invalid email: " . $userEmail);
+        ErrorResponse::makeErrorResponse(400, "Invalid email: $userEmail");
     }
 }
 
@@ -45,7 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 $userDao->updateUser($newEmail, $role, $email);
                 $updatedUser = $userDao->getUser($newEmail);
                 http_response_code(200);
-                error_log(date('l jS \of F Y h:i:s A') . " | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " succesfully used 'update user' endpoint, updated user : " . json_encode($updatedUser) . "\n", 3, $_ENV['ADMIN_ENDPOINT_LOG']);
+                error_log(date('l jS \of F Y h:i:s A') . " | User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " successfully used 'update user' endpoint, updated user : " . json_encode($updatedUser) . "\n", 3, $_ENV['ADMIN_ENDPOINT_LOG']);
                 echo "Updated user is: " . json_encode($updatedUser);
             } else {
                 error_log("User: " . $_SESSION['email'] . " with role: " . $_SESSION['role'] . " tried to update information of non-existent user: " . $email);
