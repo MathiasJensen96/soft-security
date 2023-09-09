cf31b2b5f5e14b359005f75d8bd2a09d79ca31ad
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-10 00:36:37 +0200

    fix sql injection createOrder.php

--- src/views/createOrder.php
+++ src/views/createOrder.php
@@ -1,8 +1,10 @@
-<?php include __DIR__ . "/../db/dbconn.php"?>
-<?php include __DIR__ . "/../entities/orders.php"?>
-<?php include __DIR__ . "/../entities/users.php"?>
-<?php include __DIR__ . "/../entities/orderlines.php"?>
 <?php
+
+require_once __DIR__ . "/../db/dbconn.php";
+require_once __DIR__ . "/../entities/orders.php";
+require_once __DIR__ . "/../entities/users.php";
+require_once __DIR__ . "/../entities/orderlines.php";
+
 session_start();
 
 $productId = htmlspecialchars($_POST['productId']);
@@ -10,22 +12,24 @@ $quantity = htmlspecialchars($_POST['quantity']);
 $email = $_SESSION['email'];
 
 if($userconn) {
-    $userconn->query("INSERT INTO securitydb.order (date, User_email) VALUES ((SELECT SYSDATE()),'$email')");
+    $stmt = $userconn->prepare("INSERT INTO securitydb.order (date, User_email) VALUES (NOW(),?)");
+    $stmt->execute([$email]);
 
     $sql = "SELECT LAST_INSERT_ID()";
     $getLastId = $userconn->query($sql);
     $lastID = $getLastId->fetch();
 
-    $result = $userconn->query("SELECT * FROM securitydb.order WHERE id = '$lastID[0]'");
+    $stmt = $userconn->prepare("SELECT * FROM securitydb.order WHERE id = ?");
+    $result = $stmt->execute([$lastID[0]]);
     
     if($result) {
-        header("Content-Type: application/json");
-        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $orderline = new orderlines($productId, $lastID[0], $quantity);
             $order = new orders($row['id'], $row['status'], $row['date'], $row['User_email'], $orderline);
         }
+        $stmt = $userconn->prepare("INSERT INTO securitydb.orderline (productId, orderId, quantity) VALUES (?, ?, ?)");
+        $stmt->execute([$productId, $lastID[0], $quantity]);
+        header("Content-Type: application/json");
         echo json_encode($order, JSON_PRETTY_PRINT);
-        $userconn->query("INSERT INTO securitydb.orderline (productId, orderId, quantity) VALUES ($productId, $lastID[0], $quantity)");
     }
 }
-?>
