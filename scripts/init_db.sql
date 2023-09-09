15122d348d196f99b56fac974f4c2a1e055a7138
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-09 23:41:21 +0200

    autoincrement order id

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -14,7 +14,7 @@ CREATE TABLE `product` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `order` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `status` varchar(45) DEFAULT NULL,
   `date` varchar(45) DEFAULT NULL,
   `User_email` varchar(50) NOT NULL,
