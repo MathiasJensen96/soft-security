f98bb7ca6a8d279fb01ddacfb9a1a9b65b1c6f2c
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-14 10:21:36 +0200

    add left links to navbar

--- src/views/index.php
+++ src/views/index.php
@@ -7,8 +7,15 @@
     </head>
     <body>
         <div class="topnav">
-          <a href="/login">Login</a>
-          <a href="/register">Register</a>
+            <div class="nav-left">
+                <a class="active" href="/">Home</a>
+                <a href="products">Products</a>
+                <a href="orders">Orders</a>
+            </div>
+            <div class="nav-right">
+                <a href="/login">Login</a>
+                <a href="/register">Register</a>
+            </div>
         </div>
         <div style="padding-left:16px">
           <h1>Hello World!</h1>
