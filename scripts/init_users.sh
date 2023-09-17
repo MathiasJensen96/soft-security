299c5b4e498c00137c81b8b497cf69b632fbcee9
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-17 18:23:29 +0200

    removes create product table privilege from database user

--- scripts/init_users.sh
+++ scripts/init_users.sh
@@ -3,9 +3,10 @@ set -e
 
 mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
 CREATE USER 'user'@'%' IDENTIFIED BY '$USER_PASSWORD';
-GRANT SELECT, INSERT ON securitydb.* TO 'user'@'%';
-GRANT UPDATE, DELETE ON securitydb.order TO 'user'@'%';
-GRANT UPDATE, DELETE ON securitydb.orderline TO 'user'@'%';
+GRANT SELECT ON securitydb.* TO 'user'@'%';
+GRANT INSERT ON securitydb.user TO 'user'@'%';
+GRANT INSERT, UPDATE, DELETE ON securitydb.order TO 'user'@'%';
+GRANT INSERT, UPDATE, DELETE ON securitydb.orderline TO 'user'@'%';
 
 CREATE USER 'admin'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';
 GRANT INSERT, UPDATE, SELECT, DELETE ON securitydb.* TO 'admin'@'%';
